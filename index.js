f4a94f1d6e51605a1d1e8794b570cc08eefee8f4
aimunNaharr <aimunnaharr@gmail.com>
2023-03-25 23:23:20 +0600

    socket-io implemented

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import app from "./app.js";
 import http from "http";
 import * as dotenv from 'dotenv' 
 import mongoose from "mongoose";
+import { Server } from "socket.io";
+
 
 dotenv.config()
 
 const port = process.env.PORT || 8000;
 const server = http.createServer(app); //creating the server
 
+
 const DB=process.env.DBURI.replace('password', process.env.PASSWORD)
+
 mongoose.connect(DB, {
 	// useNewUrlParser: true,
 	// useCreateIndex: true,
@@ -23,3 +27,26 @@ mongoose.connect(DB, {
 server.listen(port, () => {
 	console.log(`app is running on port: ${port}`);
 });
+
+// Created a Socket.io server 
+const io=new Server(server, {
+	cors:{
+		origin: 'http://127.0.0.1:5173',
+		credentials:  true,
+	}
+})
+
+global.onlineUsers=new Map()
+
+io.on("connection", (socket)=>{
+	global.chatSocket=socket;
+	socket.on('add-user', (userId)=>{
+		onlineUsers.set(userId, socket.id)
+	})
+	socket.on('send-msg', (data)=>{
+		const receiver=onlineUsers.get(data.to);
+		if(receiver){
+			socket.to(receiver).emit('rcv-msg', data.message)
+		}
+	})
+})
